81fab9f1cdb0a14bbbf7bd5a6e834037d93d07f0
Myles Burt <myles_burt@rocketmail.com>
2021-02-21 23:07:38 +1030

    backend opertaional.

--- server/server.js
+++ server/server.js
@@ -9,9 +9,13 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on Port: ${PORT}`));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
 app.use(express.json());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect(
   process.env.MONGODB_URI,
