80e5d13a14f1892cf939feebcdf7c2d00954e68b
Myles Burt <myles_burt@rocketmail.com>
2021-02-24 17:52:50 +1030

    Navbar and BioCard components completed with function social media icon links and an added assets folder to store a profile pic.

--- portfolioapp/src/components/BioCard/index.js
+++ portfolioapp/src/components/BioCard/index.js
@@ -1,83 +1,118 @@
 import React from "react";
+import profilePic from "../assets/portfolio_image.jpg";
 
 function BioCard() {
   return (
-    <section className="bg-white dark:bg-gray-800">
-      <div className="container px-6 py-8 mx-auto">
-        <div className="items-center lg:flex">
-          <div className="lg:w-1/2">
-            <h2 className="text-3xl font-bold text-gray-800 dark:text-gray-100">
-              Who I am
-            </h2>
-
-            <p className="mt-4 text-gray-500 dark:text-gray-400 lg:max-w-md">
-              Hi I am jane , software engineer{" "}
-              <a
-                className="font-bold text-indigo-600 dark:text-indigo-400"
-                href="#"
-              >
-                @BakaTeam
-              </a>{" "}
-              , Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum
-              in sed non alias, fugiat, commodi nemo ut fugit corrupti dolorem
-              sequi ex veniam consequuntur id, maiores beatae ipsa omnis
-              aliquam?
-            </p>
-
-            <div className="flex items-center mt-6 -mx-2">
-              <a className="mx-2" href="#" aria-label="Twitter">
-                <svg
-                  className="w-5 h-5 text-gray-700 fill-current dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 512 512"
+    <section className="bg-gray-800 text-white body-font">
+      <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-5xl text-5xl mb-4 font-medium text-white">
+            Myles Burt
+          </h1>
+          <h2 className="title-font text-xl italic">
+            Journalist - Full Stack Developer
+          </h2>
+          <br />
+          <p className="mb-8 leading-relaxed">
+            Full Stack Developer utilising skills gained through regional
+            journalism background to build audience orientated and user-friendly
+            websites. Recently earned a certificate in Full Stack Development
+            through the University of Sydney, equipped with skills and knowledge
+            in HTML, CSS, JavaScript, dynamic and responsive web design. I am an
+            energetic, charismatic, highly communicative and cooperative worker
+            that enjoys CSS and JavaScript to make aesthetic and productive
+            webpages. I have built a strong work ethic having worked as a
+            lifeguard during my three-year Bachelor of Media (Journalism) degree
+            with the University of Adelaide. Continuing this ethic during my
+            Full Stack Developer certificate, working full-time as a regional
+            journalist during my studies. I am eager to expand my knowledge in
+            web development within the professional field, and able to work in
+            teams or independently. I am excited to earn employment in a growing
+            and supportive industry, to provide solutions to web development
+            issues.
+          </p>
+          <div>
+            <div className="flex justify-center">
+              <div className="flex items-center mx-2">
+                <a
+                  className="mx-2"
+                  href="https://twitter.com/BreakingBurt"
+                  aria-label="Twitter"
                 >
-                  <path d="M492 109.5c-17.4 7.7-36 12.9-55.6 15.3 20-12 35.4-31 42.6-53.6-18.7 11.1-39.4 19.2-61.5 23.5C399.8 75.8 374.6 64 346.8 64c-53.5 0-96.8 43.4-96.8 96.9 0 7.6.8 15 2.5 22.1-80.5-4-151.9-42.6-199.6-101.3-8.3 14.3-13.1 31-13.1 48.7 0 33.6 17.2 63.3 43.2 80.7-16-.4-31-4.8-44-12.1v1.2c0 47 33.4 86.1 77.7 95-8.1 2.2-16.7 3.4-25.5 3.4-6.2 0-12.3-.6-18.2-1.8 12.3 38.5 48.1 66.5 90.5 67.3-33.1 26-74.9 41.5-120.3 41.5-7.8 0-15.5-.5-23.1-1.4C62.8 432 113.7 448 168.3 448 346.6 448 444 300.3 444 172.2c0-4.2-.1-8.4-.3-12.5C462.6 146 479 129 492 109.5z" />
-                </svg>
-              </a>
+                  <svg
+                    className="w-5 h-5 text-white fill-current hover:text-gray-600"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
+                  </svg>
+                </a>
 
-              <a className="mx-2" href="#" aria-label="Facebook">
-                <svg
-                  className="w-5 h-5 text-gray-700 fill-current dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 512 512"
+                <a
+                  className="mx-2"
+                  href="https://www.facebook.com/myles.burt"
+                  aria-label="Facebook"
                 >
-                  <path d="M426.8 64H85.2C73.5 64 64 73.5 64 85.2v341.6c0 11.7 9.5 21.2 21.2 21.2H256V296h-45.9v-56H256v-41.4c0-49.6 34.4-76.6 78.7-76.6 21.2 0 44 1.6 49.3 2.3v51.8h-35.3c-24.1 0-28.7 11.4-28.7 28.2V240h57.4l-7.5 56H320v152h106.8c11.7 0 21.2-9.5 21.2-21.2V85.2c0-11.7-9.5-21.2-21.2-21.2z" />
-                </svg>
-              </a>
+                  <svg
+                    className="w-5 h-5 text-white fill-current hover:text-gray-600"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-3 7h-1.924c-.615 0-1.076.252-1.076.889v1.111h3l-.238 3h-2.762v8h-3v-8h-2v-3h2v-1.923c0-2.022 1.064-3.077 3.461-3.077h2.539v3z" />
+                  </svg>
+                </a>
 
-              <a className="mx-2" href="#" aria-label="Linkden">
-                <svg
-                  className="w-5 h-5 text-gray-700 fill-current dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 512 512"
+                <a
+                  className="mx-2"
+                  href="https://www.instagram.com/myles_burt/?hl=en"
+                  aria-label="Instagram"
                 >
-                  <path d="M417.2 64H96.8C79.3 64 64 76.6 64 93.9V415c0 17.4 15.3 32.9 32.8 32.9h320.3c17.6 0 30.8-15.6 30.8-32.9V93.9C448 76.6 434.7 64 417.2 64zM183 384h-55V213h55v171zm-25.6-197h-.4c-17.6 0-29-13.1-29-29.5 0-16.7 11.7-29.5 29.7-29.5s29 12.7 29.4 29.5c0 16.4-11.4 29.5-29.7 29.5zM384 384h-55v-93.5c0-22.4-8-37.7-27.9-37.7-15.2 0-24.2 10.3-28.2 20.3-1.5 3.6-1.9 8.5-1.9 13.5V384h-55V213h55v23.8c8-11.4 20.5-27.8 49.6-27.8 36.1 0 63.4 23.8 63.4 75.1V384z" />
-                </svg>
-              </a>
+                  <svg
+                    className="w-5 h-5 text-white fill-current hover:text-gray-600"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
+                  </svg>
+                </a>
 
-              <a className="mx-2" href="#" aria-label="Github">
-                <svg
-                  className="w-5 h-5 text-gray-700 fill-current dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 512 512"
+                <a
+                  className="mx-2"
+                  href="https://www.linkedin.com/in/myles-burt-95ab5a19a/"
+                  aria-label="LinkedIn"
                 >
-                  <path d="M256 32C132.3 32 32 134.9 32 261.7c0 101.5 64.2 187.5 153.2 217.9 1.4.3 2.6.4 3.8.4 8.3 0 11.5-6.1 11.5-11.4 0-5.5-.2-19.9-.3-39.1-8.4 1.9-15.9 2.7-22.6 2.7-43.1 0-52.9-33.5-52.9-33.5-10.2-26.5-24.9-33.6-24.9-33.6-19.5-13.7-.1-14.1 1.4-14.1h.1c22.5 2 34.3 23.8 34.3 23.8 11.2 19.6 26.2 25.1 39.6 25.1 10.5 0 20-3.4 25.6-6 2-14.8 7.8-24.9 14.2-30.7-49.7-5.8-102-25.5-102-113.5 0-25.1 8.7-45.6 23-61.6-2.3-5.8-10-29.2 2.2-60.8 0 0 1.6-.5 5-.5 8.1 0 26.4 3.1 56.6 24.1 17.9-5.1 37-7.6 56.1-7.7 19 .1 38.2 2.6 56.1 7.7 30.2-21 48.5-24.1 56.6-24.1 3.4 0 5 .5 5 .5 12.2 31.6 4.5 55 2.2 60.8 14.3 16.1 23 36.6 23 61.6 0 88.2-52.4 107.6-102.3 113.3 8 7.1 15.2 21.1 15.2 42.5 0 30.7-.3 55.5-.3 63 0 5.4 3.1 11.5 11.4 11.5 1.2 0 2.6-.1 4-.4C415.9 449.2 480 363.1 480 261.7 480 134.9 379.7 32 256 32z" />
-                </svg>
-              </a>
-            </div>
-          </div>
+                  <svg
+                    className="w-5 h-5 text-white fill-current hover:text-gray-600"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
+                  </svg>
+                </a>
 
-          <div className="mt-8 lg:mt-0 lg:w-1/2">
-            <div className="flex items-center justify-center lg:justify-end">
-              <div className="max-w-lg">
-                <img
-                  className="object-cover object-center w-full h-64 rounded-md shadow"
-                  src="https://images.unsplash.com/photo-1484399172022-72a90b12e3c1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80"
-                  alt=""
-                />
+                <a
+                  className="mx-2"
+                  href="https://github.com/mylesburt"
+                  aria-label="Github"
+                >
+                  <svg
+                    className="w-5 h-5 text-white fill-current hover:text-gray-600"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+                  </svg>
+                </a>
               </div>
             </div>
           </div>
+        </div>
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+          <img
+            className="object-cover object-center rounded"
+            alt="hero"
+            src={profilePic}
+          />
         </div>
       </div>
     </section>
