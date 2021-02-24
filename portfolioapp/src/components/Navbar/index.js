b14ac4670a7373a6be98eaf9f9b35f34c2100093
Myles Burt <myles_burt@rocketmail.com>
2021-02-24 21:59:02 +1030

    Navbar customised.

--- portfolioapp/src/components/Navbar/index.js
+++ portfolioapp/src/components/Navbar/index.js
@@ -7,17 +7,19 @@ function Navbar() {
         <a className="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center text-gray-900 lg:items-center lg:justify-center mb-4 md:mb-0">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            stroke="currentColor"
-            strokelinecap="round"
-            strokelinejoin="round"
-            strokewidth="2"
-            className="w-10 h-10 text-white p-2 bg-green-500 rounded-full"
+            className="w-10 h-10 text-green-400 p-2 fill-current"
             viewBox="0 0 24 24"
           >
-            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+            <path d="M24 10.935v2.131l-10 4.934v-2.23l7.64-3.77-7.64-3.779v-2.221l10 4.935zm-24 0v2.131l10 4.934v-2.23l-7.64-3.77 7.64-3.779v-2.221l-10 4.935z"></path>
           </svg>
-          <span className="ml-3 text-white text-xl">MB Portfolio</span>
+          <span className="text-white text-xl">MB Portfolio</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-10 h-10 text-green-400 p-2 fill-current"
+            viewBox="0 0 24 24"
+          >
+            <path d="M24 10.935v2.131l-8 3.947v-2.23l5.64-2.783-5.64-2.79v-2.223l8 3.948zm-16 3.848l-5.64-2.783 5.64-2.79v-2.223l-8 3.948v2.131l8 3.947v-2.23zm7.047-10.783h-2.078l-4.011 16h2.073l4.016-16z"></path>
+          </svg>
         </a>
       </div>
     </header>
