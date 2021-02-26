db4d464796b444d95ffcb13c119fa0f5d96e1ea9
Myles Burt <myles_burt@rocketmail.com>
2021-02-26 19:00:09 +1030

    routes required

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const routes = require("./routes");
 
 dotenv.config();
 
@@ -10,11 +11,13 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("portfolioapp/build"));
 }
+
+app.use(routes);
 
 mongoose.connect(
   process.env.MONGODB_URI,
