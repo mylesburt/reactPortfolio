2f4fa6ad09c71b7cc2dff0089e954880c7608946
Myles Burt <myles_burt@rocketmail.com>
2021-02-26 12:45:09 +1030

    pushing changes

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
-import cors from "cors";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -10,17 +9,18 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on Port: ${PORT}`));
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("portfolioapp/build"));
   const path = require("path");
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(
+      path.resolve(__dirname, "portfolioapp", "build", "index.html")
+    );
   });
 }
 
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use("/api", routes);
 
 mongoose.connect(
   process.env.MONGODB_URI,
